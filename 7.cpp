a1ef58198a4abfce1d7b99526a0ff63d779a555d
Kajal-0912 <21f1006863@student.onlinedegree.iitm.ac.in>
2022-01-26 20:20:21 +0530

    duh

---
+++ 7.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+string converttoupper(string s){
+    string res ="";
+
+    for(int i=0;i<s.length();i++){
+        char c=s[i];
+        if(c-'a'>=0 && c-'a'<=25){
+            c = c - 'a' + 'A';
+        }
+        res.push_back(c);
+    }
+    return res;
+}
+
+int main(){
+
+    string s;
+    cin>>s;
+    string ans;
+    ans = converttoupper(s);
+    cout<<ans;
+    return 0;
+}
